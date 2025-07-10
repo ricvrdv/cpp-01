b7e12470157d9c808e746d3c08bb25831b51622a
ricvrdv <costagarcia.r95@gmail.com>
2025-07-10 13:25:12 +0100

    feat(ex03): create main()

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rjesus-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 13:13:57 by rjesus-d          #+#    #+#             */
-/*   Updated: 2025/07/10 13:15:28 by rjesus-d         ###   ########.fr       */
+/*   Updated: 2025/07/10 13:24:30 by rjesus-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,22 @@
 
 int	main()
 {
+	{
+		Weapon	club = Weapon("crude spiked club");
+		
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}	
+	{
+		Weapon	club = Weapon("crude spiked club");
+
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
 	return (0);
 }
