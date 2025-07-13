9bfee25d69d8e49f6fac652271b51d609a9165e5
Ricardo Garcia <costagarcia.r95@gmail.com>
2025-07-13 02:41:49 +0100

    feat(ex06): add working main and Harl.cpp and .hpp

---
+++ ex06/Harl.cpp
@@ -0,0 +1,43 @@
+#include "Harl.hpp"
+
+Harl::Harl( void ) {}
+
+Harl::~Harl( void ) {}
+
+void	Harl::debug ( void )
+{
+    std::cout << "I'm trying to DEBUG this code but I'm going anywhere." << std::endl;
+}
+
+void	Harl::info( void )
+{
+    std::cout << "I don't know maybe I got the INFO wrong." << std::endl;
+}
+
+void	Harl::warning( void )
+{
+    std::cout << "I could use that as a WARNING. But I'm not concerned anymore." << std::endl;
+}
+
+void	Harl::error( void )
+{
+    std::cout << "This is a critical ERROR. I'm leaving." << std::endl;
+}
+
+void	Harl::complain( std::string level )
+{
+    typedef void	(Harl::*funcPtr)();
+
+	std::string	levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	funcPtr	functions[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+		{
+			(this->*functions[i])();
+			return ;
+		}
+	}
+	std::cout << "Unknown level: " << level << std::endl;
+}
---
+++ ex06/main.cpp
@@ -0,0 +1,45 @@
+#include "Harl.hpp"
+
+static void	printMessage(Harl &harl, const std::string &level)
+{
+	std::cout << "[ " << level << " ]" << std::endl;
+	harl.complain(level);
+	std::cout << std::endl;
+}
+
+int	main( int argc, char **argv )
+{
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./harlFilter <LEVEL>" << std::endl;
+		return (1);
+	}
+	
+	std::string	levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int	index = -1;
+	for (int i = 0; i < 4; i++)
+	{
+		if (argv[1] == levels[i])
+		{
+			index = i;
+			break ;
+		}
+	}
+
+	Harl		harl;
+	switch (index)
+	{
+		case 0:
+			printMessage(harl, "DEBUG");
+		case 1:
+			printMessage(harl, "INFO");
+		case 2:
+			printMessage(harl, "WARNING");
+		case 3:
+			printMessage(harl, "ERROR");
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+	return (0);
+}
